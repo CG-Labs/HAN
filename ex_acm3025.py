Fix tensor reshaping for softmax cross-entropy operation

ex_acm3025.py
@@ -144,9 +144,11 @@ with tf.Graph().as_default():
 
     # cal masked_loss
     logits_shape = tf.shape(logits)
-    log_resh = tf.reshape(logits, [-1, logits_shape[-1]])
-    lab_resh = tf.reshape(lbl_in, [-1, logits_shape[-1]])
-    msk_resh = tf.reshape(msk_in, [-1])
+    # Reshape logits to be a 2-D tensor with shape [batch_size * nb_nodes, nb_classes]
+    # and labels to be a 1-D tensor of length [batch_size * nb_nodes]
+    log_resh = tf.reshape(logits, [batch_size * nb_nodes, nb_classes])
+    lab_resh = tf.reshape(lbl_in, [batch_size * nb_nodes])
+    msk_resh = tf.reshape(msk_in, [batch_size * nb_nodes])
     loss = model.masked_softmax_cross_entropy(log_resh, lab_resh, msk_resh)
     accuracy = model.masked_accuracy(log_resh, lab_resh, msk_resh)
     # optimzie