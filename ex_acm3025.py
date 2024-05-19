Fix broadcasting shape issue in attn_head and reshape bias_mat dynamically

ex_acm3025.py
@@ -81,12 +81,13 @@ features = 1    # The number of features used for prediction, here it's just the
 
 ftr_in = tf.keras.Input(shape=(timesteps, features), name='ftr_in')
 
-# Create a simple chain-like adjacency matrix for the Bitcoin data
-adjacency_matrix = np.eye(timesteps, k=1) + np.eye(timesteps, k=-1)
+# Correct the number of nodes to match the actual graph size for the bias matrix
+nb_nodes = 496  # This should be set to the actual number of nodes in the graph
+adjacency_matrix = np.eye(nb_nodes, k=1) + np.eye(nb_nodes, k=-1)
 adjacency_matrix = np.array([adjacency_matrix])  # Add batch dimension
 
-# Compute the bias matrix using the adjacency matrix
-bias_mat = process.adj_to_bias(adjacency_matrix, [adjacency_matrix.shape[1]], nhood=1)
+# Compute the bias matrix using the adjacency matrix with the correct shape
+bias_mat = process.adj_to_bias(adjacency_matrix, [nb_nodes], nhood=1)
 
 logits = GAT.inference(ftr_in, nb_classes=1, nb_nodes=preprocessed_data.shape[1], training=False,
                        attn_drop=0.6, ffd_drop=0.6, bias_mat=bias_mat, hid_units=hid_units,
@@ -124,6 +125,7 @@ train_mask = train_mask[np.newaxis]
 val_mask = val_mask[np.newaxis]
 test_mask = test_mask[np.newaxis]
 
+# Adjust the bias matrix to have the correct shape [1, nb_nodes, nb_nodes]
 biases_list = [process.adj_to_bias(adj, [nb_nodes], nhood=1) for adj in adj_list]
 
 print('build graph...')

utils/layers.py
@@ -8,7 +8,6 @@ class BiasAddLayer(tf.keras.layers.Layer):
         super(BiasAddLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # Set the bias size to match the last dimension of the input shape
         self.bias = self.add_weight(shape=(input_shape[-1],),
                                     initializer='zeros',
                                     name='bias')
@@ -26,7 +25,6 @@ class SqueezeLayer(tf.keras.layers.Layer):
         return tf.squeeze(inputs)
 
     def compute_output_shape(self, input_shape):
-        # Remove all singleton dimensions from the input shape, skip None dimensions
         return tuple(dim for dim in input_shape if dim is not None and dim != 1)
 
 class BroadcastToLayer(tf.keras.layers.Layer):
@@ -35,25 +33,19 @@ class BroadcastToLayer(tf.keras.layers.Layer):
         self.target_shape = target_shape
 
     def build(self, input_shape):
-        # No dynamic shape calculation needed, target_shape is provided
         super(BroadcastToLayer, self).build(input_shape)
 
     def call(self, inputs):
-        # Use the provided target_shape for broadcasting
         return tf.broadcast_to(inputs, self.target_shape)
 
     def compute_output_shape(self, input_shape):
-        # The output shape will match the provided target shape
         return self.target_shape
 
 def attn_head(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, residual=False,
               return_coef=False):
-
     with tf.name_scope('my_attn'):
         if in_drop != 0.0:
             seq = tf.keras.layers.Dropout(1.0 - in_drop)(seq, training=True)
-        # Ensure seq is three-dimensional before passing to conv1d
-        # Reshape seq to have 3 dimensions (batch_size, steps, input_dim)
         if len(seq.shape) == 4 and seq.shape[-1] == 1:
             seq = tf.reshape(seq, (-1, seq.shape[1], seq.shape[2]))
         elif len(seq.shape) == 2:
@@ -66,8 +58,11 @@ def attn_head(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, res
         logits = f_1 + tf.keras.layers.Permute((2, 1))(f_2)
         leaky_relu = tf.keras.layers.LeakyReLU()(logits)
 
-        # Instantiate BroadcastToLayer with the static output size
-        broadcast_to_layer = BroadcastToLayer(bias_mat.shape.as_list())
+        # Dynamically determine the shape of bias_mat based on seq dimensions
+        bias_shape = tf.shape(seq)[1:3]
+        bias_mat = tf.reshape(bias_mat, (1, bias_shape[0], bias_shape[1]))
+
+        broadcast_to_layer = BroadcastToLayer(list(bias_mat.shape))
         coefs = tf.keras.layers.Softmax(axis=-1)(leaky_relu + broadcast_to_layer(bias_mat))
 
         if coef_drop != 0.0:
@@ -77,23 +72,22 @@ def attn_head(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, res
 
         vals = tf.keras.layers.Dot(axes=(1, 1))([coefs, seq_fts])
 
-        # Replace the direct bias_add call with an instance of the custom layer
         bias_add_layer = BiasAddLayer()
         ret = bias_add_layer(vals)
 
-        # residual connection
+        # Apply residual connection if specified
         if residual:
             if seq.shape[-1] != ret.shape[-1]:
-                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)  # activation
+                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)
             else:
                 ret = ret + seq
 
-        # Replace the direct call to the activation function with an instance of ELULayer
+        # Instantiate ELULayer to apply ELU activation function
         elu_layer = ELULayer()
         if return_coef:
             return elu_layer(ret), coefs
         else:
-            return elu_layer(ret)  # Apply ELU activation using the custom layer
+            return elu_layer(ret)
 
 def attn_head_const_1(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=0.0, residual=False):
     adj_mat = 1.0 - bias_mat / -1e9
@@ -101,7 +95,6 @@ def attn_head_const_1(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=
         if in_drop != 0.0:
             seq = tf.keras.layers.Dropout(1.0 - in_drop)(seq, training=True)
         seq_fts = conv1d(filters=out_sz, kernel_size=1, use_bias=False)(seq)
-
 
         logits = adj_mat
         coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + bias_mat)
@@ -114,16 +107,14 @@ def attn_head_const_1(seq, out_sz, bias_mat, activation, in_drop=0.0, coef_drop=
         vals = tf.matmul(coefs, seq_fts)
         ret = tf.keras.backend.bias_add(vals)
 
-        # residual connection
         if residual:
             if seq.shape[-1] != ret.shape[-1]:
-                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)  # activation
+                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)
             else:
                 seq_fts = ret + seq
 
-        # Replace the direct call to the activation function with an instance of ELULayer
         elu_layer = ELULayer()
-        return elu_layer(ret)  # Apply ELU activation using the custom layer
+        return elu_layer(ret)
 
 def sp_attn_head(seq, out_sz, adj_mat, activation, nb_nodes, in_drop=0.0, coef_drop=0.0, residual=False):
     with tf.name_scope('sp_attn'):
@@ -132,11 +123,9 @@ def sp_attn_head(seq, out_sz, adj_mat, activation, nb_nodes, in_drop=0.0, coef_d
 
         seq_fts = conv1d(filters=out_sz, kernel_size=1, use_bias=False)(seq)
 
-        # simplest self-attention possible
         f_1 = conv1d(filters=1, kernel_size=1)(seq_fts)
         f_2 = conv1d(filters=1, kernel_size=1)(seq_fts)
-        logits = tf.sparse_add(adj_mat * f_1, adj_mat *
-                               tf.transpose(f_2, [0, 2, 1]))
+        logits = tf.sparse_add(adj_mat * f_1, adj_mat * tf.transpose(f_2, [0, 2, 1]))
         lrelu = tf.SparseTensor(indices=logits.indices,
                                 values=tf.nn.leaky_relu(logits.values),
                                 dense_shape=logits.dense_shape)
@@ -149,9 +138,6 @@ def sp_attn_head(seq, out_sz, adj_mat, activation, nb_nodes, in_drop=0.0, coef_d
         if in_drop != 0.0:
             seq_fts = tf.keras.layers.Dropout(1.0 - in_drop)(seq_fts, training=True)
 
-        # As tf.sparse_tensor_dense_matmul expects its arguments to have rank-2,
-        # here we make an assumption that our input is of batch size 1, and reshape appropriately.
-        # The method will fail in all other cases!
         coefs = tf.sparse_reshape(coefs, [nb_nodes, nb_nodes])
         seq_fts = tf.squeeze(seq_fts)
         vals = tf.sparse_tensor_dense_matmul(coefs, seq_fts)
@@ -159,45 +145,33 @@ def sp_attn_head(seq, out_sz, adj_mat, activation, nb_nodes, in_drop=0.0, coef_d
         vals.set_shape([1, nb_nodes, out_sz])
         ret = tf.keras.backend.bias_add(vals)
 
-        # residual connection
         if residual:
             if seq.shape[-1] != ret.shape[-1]:
-                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)  # activation
+                ret = ret + conv1d(filters=ret.shape[-1], kernel_size=1)(seq)
             else:
                 seq_fts = ret + seq
 
-        return activation(ret)  # activation
+        return activation(ret)
 
-# final_embed, att_val = layers.SimpleAttLayer(multi_embed, mp_att_size,
-#                                                      time_major=False,
-#                                                      return_alphas=True)
 def SimpleAttLayer(inputs, attention_size, time_major=False, return_alphas=False):
-
     if isinstance(inputs, tuple):
-        # In case of Bi-RNN, concatenate the forward and the backward RNN outputs.
         inputs = tf.concat(inputs, 2)
 
     if time_major:
-        # (T,B,D) => (B,T,D)
         inputs = tf.array_ops.transpose(inputs, [1, 0, 2])
 
-    hidden_size = inputs.shape[2].value  # D value - hidden size of the RNN layer
+    hidden_size = inputs.shape[2].value
 
-    # Trainable parameters
     w_omega = tf.Variable(tf.random_normal([hidden_size, attention_size], stddev=0.1))
     b_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
     with tf.name_scope('v'):
-        # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
-        #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
         v = tf.tanh(tf.tensordot(inputs, w_omega, axes=1) + b_omega)
 
-    # For each of the timestamps its vector of size A from `v` is reduced with `u` vector
-    vu = tf.tensordot(v, u_omega, axes=1, name='vu')  # (B,T) shape
-    alphas = tf.nn.softmax(vu, name='alphas')         # (B,T) shape
+    vu = tf.tensordot(v, u_omega, axes=1, name='vu')
+    alphas = tf.nn.softmax(vu, name='alphas')
 
-    # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
     output = tf.reduce_sum(inputs * tf.expand_dims(alphas, -1), 1)
 
     if not return_alphas: